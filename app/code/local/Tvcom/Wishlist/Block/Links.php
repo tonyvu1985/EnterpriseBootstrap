70f501f5f878757957c06d1e0fd083940306ff9b
Tony Vu <vuductrung2003@gmail.com>
2014-05-03 13:00:17 +1000

    bootstrap

--- app/code/local/Tvcom/Wishlist/Block/Links.php
+++ app/code/local/Tvcom/Wishlist/Block/Links.php
@@ -7,10 +7,11 @@
 class Tvcom_Wishlist_Block_Links extends Mage_Wishlist_Block_Links{
     protected function _createLabel($count){
         if ($count >= 1) {
-            return $this->__('My Wishlist %d', $count);
+            return $this->__("<i class='wishlist-icon fa fa-gift'></i> <span class='wishlist-text'>My Wishlist</span> %d", $count);
         } else {
-            return $this->__('My Wishlist');
+            return $this->__("<i class='wishlist-icon fa fa-gift'></i> <span class='wishlist-text'>My Wishlist</span>");
         }
     }
+   
 }
 ?>
